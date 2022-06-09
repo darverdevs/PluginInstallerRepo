6b471f6f75cbd58202ca74043c32d571386c3ede
primitt <prianshmittra@gmail.com>
2022-06-09 11:17:03 -0700

    Update - Plugins v1.0

---
+++ update.py
@@ -0,0 +1,32 @@
+import requests
+import json 
+import os
+url = "https://api.github.com/repos/darverdevs/PluginInstallerRepo/git/trees/main"
+r = requests.get(url).json()
+files = r['tree']
+i = 1
+a = ''
+with open('names.txt', "a") as f:
+    for file in files:
+        if file['type'] == 'blob':
+            split = file['path'].split('.')
+            num = i%2
+            if num == 0:
+                if split[1] == "jar":
+                    form = str(i) + ". " + split[0]
+                    lasta = a
+                    newa = a +form + "\t"
+                    a = newa
+                    f.write(a + "\n")
+                    a = ''
+                    i+=1
+                else:
+                    continue
+            else:
+                if split[1] == "jar":
+                    form = str(i) + ". " + split[0]
+                    lasta = a
+                    newa = a +form + "\t"
+                    a = newa
+                    i+=1
+            
